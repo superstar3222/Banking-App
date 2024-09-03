0704413dad4255effa81b54af184d8fcdca420af
superstar3222 <3222superstar@gmail.com>
2024-09-03 17:23:33 -0400

    Save progress

---
+++ lib/utils/layouts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Layouts {
+  static getTheme(BuildContext context) {
+    return Theme.of(context);
+  }
+  static getSize(BuildContext context) {
+    return MediaQuery.of(context).size;
+  }
+
+}
+
+// Auto-generated comment for change 8
